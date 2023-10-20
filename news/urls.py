35d016fd1c9d58f3bc51526185a414302086bb7b
Levy Araujo <levy4raujo@gmail.com>
2023-10-20 16:26:31 -0300

    [Requisito 6 concluído]
    Criando Template Forms Category.

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from news.views import news_home, news_details
+from news.views import news_home, news_details, create_categories_form
 
 
 urlpatterns = [
     path("", news_home, name="home-page"),
-    path("news/<int:id>", news_details, name="news-details-page")
+    path("news/<int:id>", news_details, name="news-details-page"),
+    path("categories/", create_categories_form, name="categories-form")
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from news.models.news_model import News
+from news.models.category_model import Categories
+from news.forms import CreateCategoryForm
 
 
 def news_home(request):
@@ -14,3 +16,17 @@ def news_details(request, id):
         "news": News.objects.get(id=id)
     }
     return render(request, 'news_details.html', context)
+
+
+def create_categories_form(request):
+    if request.method == "POST":
+        form = CreateCategoryForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            Categories.objects.create(name=name)
+            return redirect('home-page')
+    form = CreateCategoryForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'categories_form.html', context)
