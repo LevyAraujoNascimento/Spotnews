2b535c7aff7a872746933d5c4f26c5789073e5a4
Levy Araujo <levy4raujo@gmail.com>
2023-10-20 10:11:04 -0300

    [Requisito 3 concluído]
    Criando Model para News.

---
+++ news/models/news_model.py
@@ -0,0 +1,28 @@
+from django.db import models
+from news.models.user_model import Users
+from news.models.category_model import Categories
+from news.models.validators import validate_title_words
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        validators=[validate_title_words]
+    )
+    content = models.TextField()
+    author = models.ForeignKey(
+        Users,
+        on_delete=models.CASCADE,
+        related_name="news"
+    )
+    created_at = models.DateTimeField()
+    image = models.ImageField(
+        upload_to="img", null=True, blank=True
+    )
+    categories = models.ManyToManyField(
+        Categories,
+        related_name="news"
+    )
+
+    def __str__(self):
+        return self.title
