f5e0a3110d93f9e10bad9c15a8a8326916ba2967
Levy Araujo <levy4raujo@gmail.com>
2023-10-20 14:04:25 -0300

    [Requisito 5 concluído]
    Criando Template News Details.

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from news.views import news_home
+from news.views import news_home, news_details
 
 
 urlpatterns = [
-    path("", news_home, name="home-page")
+    path("", news_home, name="home-page"),
+    path("news/<int:id>", news_details, name="news-details-page")
 ]
--- news/views.py
+++ news/views.py
@@ -7,3 +7,10 @@ def news_home(request):
         "news": News.objects.all()
     }
     return render(request, 'home.html', context)
+
+
+def news_details(request, id):
+    context = {
+        "news": News.objects.get(id=id)
+    }
+    return render(request, 'news_details.html', context)
