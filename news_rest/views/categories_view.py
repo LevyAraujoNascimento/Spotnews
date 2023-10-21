29de593ff611700e949c3ac555206fa38a5c17ea
Levy Araujo <levy4raujo@gmail.com>
2023-10-21 11:59:50 -0300

    [Requisito 9 concluído]
    Criando DRF Users.

---
+++ news_rest/views/categories_view.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from news.models.category_model import Categories
+from news_rest.serializers.categories_serializers import CategoriesSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
