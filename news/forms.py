b62a380969e98ad2add02dc76bd64cba27dfd723
Levy Araujo <levy4raujo@gmail.com>
2023-10-21 09:55:24 -0300

    [Requisito 6 alterando]
    Trocando Forms para ModelForm.

--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,12 @@
 from django import forms
+from news.models.category_model import Categories
 
 
-class CreateCategoryForm(forms.Form):
-    name = forms.CharField(
-        max_length=200,
-        label='Nome'
-    )
+class CreateCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Categories
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"].label = "Nome"
